c801b46735310bb54c0c2b352e7ed7d27b3414e1
shijintao001 <773265313@qq,com>
2018-03-03 16:02:42 +0800

    退出功能完成

--- public/blank/js/common.js
+++ public/blank/js/common.js
@@ -18,6 +18,43 @@ $(document).ajaxStop(function () {
 });
 
 
+//二级菜单显示与隐藏
+    $('.second').prev().on("click",function () {
+        // console.log(1);
+        $(this).next().slideToggle();
+    });
+
+
+    //左侧栏隐藏 给icon-menu 注册点击事件
+    $('.icon-menu').on('click',function () {
+       // 让侧边栏隐藏
+        console.log(1);
+        $(".lt-aside").toggleClass("now");
+
+        // 让main 隐藏
+        $('.lt-main').toggleClass('now');
+    });
+
+    //退出功能
+    $('.icon-logout').on('click',function () {
+        $('#logoutModal').modal('show');
+    });
+
+    $('.btn_logout').on('click',function () {
+        // console.log(1);
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function(info){
+                if(info.success){
+                    location.href='login.html'
+                }
+            }
+        })
+
+    });
+
+
 
 
 
